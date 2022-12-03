3b839180abf237af5d384420d3fddcb4a1d9e5f6
Cameron S. Williamson <therealkey@outlook.com>
2022-12-02 21:09:45 -0800

    Added template

---
+++ src/bin/template.rs
@@ -0,0 +1,30 @@
+fn solutiona(input: &str) -> usize {
+    0
+}
+
+fn solutionb(input: &str) -> usize {
+    0
+}
+
+fn main() {
+    let text = include_str!("dayxx.txt");
+    println!("Day xa: {}", solutiona(text));
+    println!("Day xb: {}", solutionb(text));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn examplea() {
+        let text = "";
+        assert_eq!(solutiona(text), 0);
+    }
+
+    #[test]
+    fn exampleb() {
+        let text = "";
+        assert_eq!(solutionb(text), 0)
+    }
+}
